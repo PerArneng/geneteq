fac007e7ec00963e5928c702f9551cfe6c8f7a1f
Per Arneng <per.arneng@scalebit.com>
2013-05-18 22:56:45 +0200

    Created the geneteq crate

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 
-all:
-	rustc src/*.rs -o geneteq
+all: libgeneteq
+	rustc src/main.rs -o geneteq -L .
+
+libgeneteq:
+	rustc --lib --out-dir . src/geneteq.rc
 
 clean:
-	rm -rf geneteq geneteq.dSYM
+	rm -rf geneteq libgeneteq-*.dylib *.dSYM
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 
+extern mod geneteq;
+
+use geneteq::model::*;
+
 static programName: &'static str = "geneteq";
 static version: &'static str = "0.1";
 
 fn main() {
-    io::println(fmt!("%s %s", programName, version))
+    io::println(fmt!("%s %s", programName, version));
+    
+    let xx = Node {s:1};
+    io::println(fmt!("%d", xx.s));
 }
+
